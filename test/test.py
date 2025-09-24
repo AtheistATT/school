ca62912c482463471181269c61294f91f3d5e0e8
AtheistATT <adalarus@gmail.com>
2025-09-24 20:55:48 +0500

    Добавил пояснения на русском. Внес правки в код

--- test/test.py
+++ test/test.py
@@ -5,10 +5,6 @@ import random
 import datetime
 
 class question:
-    q_title = ""
-    q_right_ansver = ""
-    q_choise = []
-
     def __init__(self, q_str) -> None:
        q_list = q_str.split('|')
        self.q_title = q_list.pop(0)
@@ -17,7 +13,7 @@ class question:
        self.q_choise = q_list
 
     def ask(self):
-        u_choise = questionary.select(self.q_title, choices=self.q_choise).ask()
+        u_choise = questionary.select(self.q_title, choices=self.q_choise, instruction="Для выбора ответа используйте стрелочки и клавишу ENTER.").ask()
         if u_choise == self.q_right_ansver:
             return True
         else:
@@ -31,7 +27,7 @@ data_path = "./data/hiden/"
 file_list = os.listdir(data_path)
 test = ""
 
-test_file = questionary.select("Выберите название теста.",choices=file_list).ask()
+test_file = questionary.select("Выберите название теста.",choices=file_list, instruction="Для выбора теста используйте стрелочки и клавишу ENTER.").ask()
 
 with open(data_path + test_file, "r", encoding='utf-8') as file:
     test = file.read()
