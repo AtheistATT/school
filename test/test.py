db87aa5f30cb000c0beedfb5a104f0b9ede528f9
AtheistATT <adalarus@gmail.com>
2025-10-09 14:10:56 +0500

    Добавил подробные логи.

--- test/test.py
+++ test/test.py
@@ -11,10 +11,11 @@ class question:
        self.q_right_ansver = q_list[0]
        random.shuffle(q_list)
        self.q_choise = q_list
+       self.u_choise = ''
 
     def ask(self):
-        u_choise = questionary.select(self.q_title, choices=self.q_choise, instruction="⬆️ ⬇️ ↩️.").ask()
-        if u_choise == self.q_right_ansver:
+        self.u_choise = questionary.select(self.q_title, choices=self.q_choise, instruction="⬆️ ⬇️ ↩️.").ask()
+        if self.u_choise == self.q_right_ansver:
             return True
         else:
             return False
@@ -27,10 +28,13 @@ data_path = "./data/hiden/"
 file_list = os.listdir(data_path)
 last = ''
 user_name = ''
+detals_log = ''
 
 test_file = questionary.select("Выберите название теста.",choices=file_list, instruction="⬆️ ⬇️ ↩️").ask()
 q_size = int(input("Выберете количество вопросов в тесте\n>>>"))
 mark = input("Введите название класса\n>>>")
+
+detals_log = mark + '\n'
 
 while True:
     os.system('clear')
@@ -47,8 +51,11 @@ while True:
         q_size = len(test)
 
 
-    print(f"{user_name} {last}")
+    print(last)
     user_name = input(title + "\nВведите фамилию ↩️ >>>")
+
+    detals_log +=  user_name + '\n'
+
     random.shuffle(test)
     test = test[:q_size]
 
@@ -60,11 +67,17 @@ while True:
 
         if quest.ask():
             q_user_right += 1
+            detals_log += ' + '  
+        detals_log += quest.q_title + '(' + quest.q_right_ansver + ') Ваш ответ:'+ quest.u_choise +'\n'
+
     log_str = f"{mark} {test_file} {user_name} {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {q_user_right}/{q_max} {q_user_right/q_max * 100:.1f}%"
     print(log_str)
     last = log_str
 
     with open("data/log.txt", 'a', encoding='utf-8') as file:
         file.write(log_str + '\n')
+    with open("data/log_d.txt", 'a', encoding='utf-8') as file:
+        file.write(detals_log + '\n')
+
 
     input("Для продолжения нажмите ENTER...")
